565afc653d07a70f1b34a9bc2633b1ca7df02fb9
jmanriquec <georgemanc.28@gmail.com>
2020-04-03 20:43:53 -0500

    V10
    Actualizacion

---
+++ jquery/main.js
@@ -0,0 +1,38 @@
+$(document).ready(movil);
+
+function movil(){
+$(document).ready(function(){
+	var altura = $('.menu_bar').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu_bar').addClass('menu-fixed');
+		} else {
+			$('.menu_bar').removeClass('menu-fixed');
+		}
+	});
+
+});
+};
+
+
+
+var contador = 1;
+
+
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); 
+
+		if(contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+
+	});
